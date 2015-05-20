8ffcc0217fc82dfb5f80e9213e1a358891e0c937
Dmitry Sedov <sedov.dmitry1@gmail.com>
2015-05-21 00:28:05 +0300

    add logic  without CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'signups#new'
   get '/thanks' => 'pages#thanks'
   resources :singups
   # The priority is based upon order of creation: first created -> highest priority.
